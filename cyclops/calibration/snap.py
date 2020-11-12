0d25958395edcae25220be21f9ca2d03aab23b13
Lewis, Edward F <el17949>
2020-11-12 12:37:43 +0000

    Images names includes hostname

--- cyclops/calibration/snap.py
+++ cyclops/calibration/snap.py
@@ -4,9 +4,13 @@ import sys
 import argparse
 import os
 from imutils.video import VideoStream
+import socket
+import errno
+
+HOSTNAME = socket.gethostname()
 
 
-def snap(folder, camera_id, width, height):
+def snap(folder, width, height, snap_count):
     vs = VideoStream(usePiCamera=True, resolution=(
         width, height), framerate=80).start()
     time.sleep(2.0)
@@ -16,9 +20,7 @@ def snap(folder, camera_id, width, height):
         if error.errno != errno.EEXIST:
             raise
 
-    snap_count = 0
-
-    file_name = "%s/%s_%d_%d_" % (folder, camera_id,
+    file_name = "%s/%s_%d_%d_" % (folder, HOSTNAME,
                                   width, height)
     while(True):
         image = vs.read()
@@ -42,17 +44,18 @@ def main():
         description="Saves image from the camera. Press q to quit and spacebar to save the snapshot")
     parser.add_argument("output_folder",
                         help="Path to folder to save images in.")
-    parser.add_argument("camera_id",
-                        help="An id for this camera to keep track of calibration images.")
     parser.add_argument("--width",
                         default=640,
                         type=int)
     parser.add_argument("--height",
                         default=480,
                         type=int)
+    parser.add_argument("--count",
+                        default=0,
+                        type=int)
     args = parser.parse_args()
 
-    snap(args.output_folder, args.camera_id, args.width, args.height)
+    snap(args.output_folder, args.width, args.height, args.count)
 
     print("Files saved")
 
