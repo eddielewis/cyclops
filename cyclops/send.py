d75f3620e5e2c1558426141a504a4445a00fbda9
Lewis, Edward F <el17949@essex.ac.uk>
2021-01-08 13:06:23 +0000

    Setup server over SSH

--- cyclops/send.py
+++ cyclops/send.py
@@ -1,17 +1,23 @@
 import socket
-
+import json
 
 PORT = 5005
-PI_COUNT = 1
+# PI_HOSTNAMES = ["cyclops0.local", "cyclops1.local"]
+PI_HOSTNAMES = ["cyclops0.local"]
+
 
 ip_addresses = []
 
-for i in range(PI_COUNT):
-    hostname = "cyclops%s.local" % i
+obj = {"command": "close"}
+message = json.dumps(obj)
+message = message.encode("utf-8")
+
+for hostname in PI_HOSTNAMES:
     ip = socket.gethostbyname(hostname)
     ip_addresses.append(ip)
 
 for ip in ip_addresses:
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
-    sock.sendto(b"snap", (ip, PORT))
+
+    sock.sendto(message, (ip, PORT))
