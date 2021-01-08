b6157a8bf7ba9af96bfa2029b322df7aa93daad3
Lewis, Edward F <el17949@essex.ac.uk>
2021-01-08 13:07:50 +0000

    Automated SSH using ssh2

--- cyclops/new_ssh.py
+++ cyclops/new_ssh.py
@@ -1,4 +1,4 @@
-import os
+import time
 import socket
 
 from ssh2.session import Session
@@ -40,21 +40,32 @@ def setup(hostname):
     host_ip = socket.gethostbyname(hostname)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, 22))
+    sock.connect((host_ip, 22))
 
     session = Session()
     session.handshake(sock)
     session.userauth_password(
         USER, PASSWORD)
 
-    channel = session.open_session()
-    channels[hostname] = channel
+    return session.open_session()
 
 
-def run_server(channel):
+def run_server(hostname):
+    channel = setup(hostname)
     channel.execute("%s %s/run_server.sh" % (ENV, PROJECT_ROOT))
-    size, data = channel.read()
-    while size > 0:
-        print(data)
-        size, data = channel.read()
     channel.close()
+
+
+def kill_server(hostname):
+    print("iouesfkhbnsef")
+    channel = setup(hostname)
+    channel.execute("pkill python")
+    print("iouesfkhbnsef")
+    size, data = channel.read()
+    print(data)
+    channel.close()
+
+
+run_server("cyclops0.local")
+time.sleep(2.0)
+kill_server("cyclops0.local")
