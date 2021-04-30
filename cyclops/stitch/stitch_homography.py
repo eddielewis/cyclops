872ae77ec3689572d7486be3f128536ed4d2a2c6
Lewis, Edward F <el17949@essex.ac.uk>
2021-04-30 06:01:58 +0100

    Commented code

---
+++ cyclops/stitch/stitch_homography.py
@@ -0,0 +1,238 @@
+import argparse
+import cv2
+import numpy as np
+
+
+class Stitcher:
+    def set_params_from_file(self, fn):
+        # Loads homography matrices and camera layout from a NumPY .npz file.
+        with np.load(fn, allow_pickle=True) as f:
+            self.h_matrices = f["h_matrices"].tolist()
+            self.camera_layout = f["camera_layout"].tolist()
+
+    def set_params(self, h_matrices, camera_layout):
+        self.h_matrices = h_matrices
+        self.camera_layout = camera_layout
+
+    def stitch_h(self, frame_dict, estimate_H):
+        """
+        Horizontally stitches a set of images into one image.
+        Expects a dict of str(camera_id) and np.array(frame),
+        and boolean value for whether homography should be done or not.
+        Returnes a stitched image of the image set.
+        """
+        # Cannot stitch if the matrices or layout is not known
+        if self.h_matrices is None or self.camera_layout is None:
+            return None
+        stitched_img = frame_dict[self.camera_layout[0]]
+        # Iterates through the image pairs according to the camera layout list
+        for i in range(len(self.camera_layout)-1):
+            cam_a_id = self.camera_layout[i]
+            cam_b_id = self.camera_layout[i+1]
+
+            img_b = stitched_img
+            img_a = frame_dict[cam_b_id]
+
+            if estimate_H:
+                H = estimate_homography([img_a, img_b])
+            else:
+                # Finds the homography matrix for the image pair
+                m_id = matrix_id(cam_a_id, cam_b_id)
+                H = self.h_matrices[m_id]
+            stitched_img = stitch_pair_h([img_a, img_b], H)
+        return stitched_img
+
+    def stitch_v(self, imgs):
+        # Cannot stitch if the matrices or layout is not known
+        if self.h_matrices is None or self.camera_layout is None:
+            return None
+        stitched_img = imgs[0]
+        for i in range(1, len(self.camera_layout)-1):
+            img_b = stitched_img
+            img_a = imgs[i]
+            H = estimate_homography([img_a, img_b])
+            stitched_img = stitch_pair_v([img_a, img_b], H)
+        return stitched_img
+
+
+def matrix_id(cam_a_id, cam_b_id):
+    """
+    Used to generate a homography matrix key from camera ids.
+    Expects str(camera_id)
+    Returns str(matrix_key)
+    """
+    return cam_a_id + "_" + cam_b_id
+
+
+def estimate_homography(images, ratio=0.75, reproj_thresh=4.0):
+    # Taken from https://www.pyimagesearch.com/2016/01/25/real-time-panorama-and-image-stitching-with-opencv/
+    # unpack the images, then detect keypoints and extract
+    # local invariant descriptors from them
+    image_a, image_b = images
+    kpts_a, features_a = detect_and_describe(image_a)
+    kpts_b, features_b = detect_and_describe(image_b)
+    # match features between the two images
+    match = match_keypoints(kpts_a, kpts_b,
+                            features_a, features_b, ratio, reproj_thresh)
+    # if the match is None, then there aren't enough matched
+    # keypoints to create a panorama
+    if match is None:
+        return None
+
+    # otherwise, apply a perspective warp to stitch the images
+    # together
+    (matches, H, status) = match
+    return H
+
+
+def remove_border(stitched_img):
+    """
+    Finds contours in the image, calculates their areas and removes all but
+    the largest one from the image. This reduces the number of image fragments
+    caused by stitching.
+    Expects a stitched image.
+    Returns the image with any excess removed.
+    """
+    gray = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(
+        gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    max_a, max_a_index = 0, 0
+    for i in range(0, len(contours)):
+        a = cv2.contourArea(contours[i], False)
+        if a > max_a:
+            max_a = a
+            max_a_index = i
+    bounding_rect = cv2.boundingRect(contours[max_a_index])
+    x, y, width, height = bounding_rect
+    stitched_img = stitched_img[y:height, x:width]
+    return stitched_img
+
+
+def stitch_pair(images, H, w, h):
+    """
+    Uses homography matrix to warp the image perspective,
+    appends the other image in the pair and removes black regions
+    from the image using contours.
+    Expects two images, the homography matrix that relates them,
+    the width and height of the images.
+    Returns a stitched image.
+    """
+    img_a, img_b = images
+    stitched_img = cv2.warpPerspective(img_a, H, (w, h))
+    stitched_img[0:img_b.shape[0], 0:img_b.shape[1]] = img_b
+    stitched_img = remove_border(stitched_img)
+    return stitched_img
+
+
+def stitch_pair_h(images, H):
+    img_a, img_b = images
+    w = img_a.shape[1] + img_b.shape[1]
+    h = max(img_a.shape[0], img_b.shape[0])
+    return stitch_pair(images, H, w, h)
+
+
+def stitch_pair_v(images, H):
+    img_a, img_b = images
+    w = max(img_a.shape[0], img_b.shape[0])
+    h = img_a.shape[1] + img_b.shape[1]
+    return stitch_pair(images, H, w, h)
+
+
+def detect_and_describe(image):
+    # Taken from https://www.pyimagesearch.com/2016/01/25/real-time-panorama-and-image-stitching-with-opencv/
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # detect and extract features from the image
+    descriptor = cv2.SIFT_create()
+    (kps, features) = descriptor.detectAndCompute(image, None)
+    # convert the keypoints from KeyPoint objects to NumPy
+    # arrays
+    kps = np.float32([kp.pt for kp in kps])
+    # return a tuple of keypoints and features
+    return (kps, features)
+
+
+def match_keypoints(kps_a, kps_b, features_a, features_b,
+                    ratio, reproj_thresh):
+    # Taken from https://www.pyimagesearch.com/2016/01/25/real-time-panorama-and-image-stitching-with-opencv/
+    # compute the raw matches and initialize the list of actual
+    # matches
+    matcher = cv2.DescriptorMatcher_create("BruteForce")
+    raw_matches = matcher.knnMatch(features_a, features_b, 2)
+    matches = []
+    # loop over the raw matches
+    for m in raw_matches:
+        # ensure the distance is within a certain ratio of each
+        # other (i.e. Lowe's ratio test)
+        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+            matches.append((m[0].trainIdx, m[0].queryIdx))
+
+    # computing a homography requires at least 4 matches
+    if len(matches) > 4:
+        # construct the two sets of points
+        ptsA = np.float32([kps_a[i] for (_, i) in matches])
+        ptsB = np.float32([kps_b[i] for (i, _) in matches])
+        # compute the homography between the two sets of points
+        (h_matrix, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,
+                                                reproj_thresh)
+        # return the matches along with the homograpy matrix
+        # and status of each matched point
+        return (matches, h_matrix, status)
+    # otherwise, no homograpy could be computed
+    return None
+
+
+def calc_h_matrices(frame_dict):
+    """
+    Calculates homography matrices for each frame pair.
+    Expects a dict of str(camera_id) and np,array(frame)
+    Returns dict of str(camera_id) and np.array(h_matrix)
+    """
+    h_matrices = {}
+
+    for i in range(len(CAMERA_LAYOUT)-1):
+        cam_a_id = CAMERA_LAYOUT[i]
+        cam_b_id = CAMERA_LAYOUT[i+1]
+
+        imgs = [frame_dict[cam_a_id], frame_dict[cam_b_id]]
+        H = estimate_homography(imgs)
+        m_id = matrix_id(cam_a_id, cam_b_id)
+        h_matrices[m_id] = H
+    return h_matrices
+
+
+# Defines the camwera layout so the images can be stitched in the correct order
+CAMERA_LAYOUT = ["cyclops1.local", "cyclops0.local"]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("left_img",
+                        type=str)
+    parser.add_argument("right_img",
+                        type=str)
+    args = parser.parse_args()
+
+    left_img = cv2.imread(args.left_img)
+    right_img = cv2.imread(args.right_img)
+
+    frame_dict = {
+        "cyclops0.local": left_img,
+        "cyclops1.local": right_img
+    }
+
+    h_matrices = calc_h_matrices(frame_dict)
+    stitcher = Stitcher()
+    stitcher.set_params(h_matrices, CAMERA_LAYOUT)
+
+    h_img = stitcher.stitch_h(frame_dict)
+    cv2.imshow("h img", h_img)
+
+    v_img = stitcher.stitch_v([right_img, left_img])
+    cv2.imshow("v img", v_img)
+
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
--- cyclops/stitch/undistort.py
+++ cyclops/stitch/undistort.py
@@ -2,21 +2,30 @@ import argparse
 import numpy as np
 import cv2
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("image_path",
-                    help="Path to the image",
-                    type=str)
-parser.add_argument("camera_matrix_path",
-                    help="Path to saved camera matrix",
-                    type=str)
-args = parser.parse_args()
-
-with np.load(args.camera_matrix_path) as f:
-    camera_matrix, dist_coefs, rvecs, tvecs = [f[i] for i in (
-        'camera_matrix', 'dist_coefs', 'rvecs', 'tvecs')]
-img = cv2.imread(args.image_path)
+"""
+Undistorts the image supplied in program arugment with camera matrix specified.
+"""
 
 
-undistorted_img = cv2.undistort(img, camera_matrix, dist_coefs)
-cv2.imwrite(args.image_path[:-4] + "_undistorted" + args.image_path[-4:], img)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_path",
+                        help="Path to the image",
+                        type=str)
+    parser.add_argument("camera_matrix_path",
+                        help="Path to saved camera matrix",
+                        type=str)
+    args = parser.parse_args()
+
+    with np.load(args.camera_matrix_path) as f:
+        camera_matrix, dist_coefs, rvecs, tvecs = [f[i] for i in (
+            'camera_matrix', 'dist_coefs', 'rvecs', 'tvecs')]
+    img = cv2.imread(args.image_path)
+
+    undistorted_img = cv2.undistort(img, camera_matrix, dist_coefs)
+    cv2.imwrite(args.image_path[:-4] +
+                "_undistorted" + args.image_path[-4:], img)
+
+
+if __name__ == "__main__":
+    main()
